c0bfb01ef1274f8eb9dd43b490386a80c3911ac3
bixingjian <bixingjian19@163.com>
2022-11-20 21:13:03 -0800

    update interface, add and update generics

--- demos/src/Interfaces/BankAccountTest.java
+++ demos/src/Interfaces/BankAccountTest.java
@@ -2,7 +2,7 @@ package Interfaces;
 
 public class BankAccountTest {
     public static void main(String[] args) {
-        BankAccount.Money money = new BankAccount.Money();
+        BankAccount.Money money = new BankAccount.Money(); // For static inner class, you can directly access it.
 
 
     }
